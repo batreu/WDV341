d1902fc2844c425c871054a6c1dd08788067d68e
batreu <98291615+batreu@users.noreply.github.com>
2022-01-23 18:34:20 -0600

    added starting php command assignments

---
+++ WDV341/helloworld.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>WDV341 intro</title>
+</head>
+<body>
+<h1>WDV341 Intro PHP</h1>
+<p>hello world! :) </p>
+<?php
+echo"<h3>Hello World! From PHP!</h3>";
+?>
+</body>
+</html>
